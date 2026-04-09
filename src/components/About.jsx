d5bedee45d26a649154a6ddd06e8b82cf456d16b
Chaimaa101 <afkirchaimaa36@gmail.com>
2026-04-09 02:26:45 +0100

    edit resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -52,7 +52,7 @@ export const About = () => {
               </a>
 
               <a
-                href="/Chaimaa-Akir(Francais).pdf"
+                href="/Chaimaa-Afkir(Francais).pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
